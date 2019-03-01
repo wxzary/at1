31919eee60087c14c6177986ebc5311cdb71609b
Mariana <48133398+wxzary@users.noreply.github.com>
2019-03-01 14:48:51 -0300

    atividade3

---
+++ at3.php
@@ -0,0 +1,11 @@
+<?php
+
+print"insira o valor do seu salário: ";
+$salario=(int)fgets(STDIN);
+
+print"insira a porcentagem de aumento: ";
+$porcento=(int)fgets(STDIN);
+
+$salarionovo = $salario*$porcento/100 ;
+
+print "o total de aumento é: $salarionovo";
